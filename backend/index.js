cd78216a9622f1b5505abdf73815193d4f4a81f2
Nayab Naeem <nayabnaeem666@gmail.com>
2026-08-09 21:38:44 +0500

    following fixing CORS issue

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://full-stack-library-management-syste-xi.vercel.app"
-    ],
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
